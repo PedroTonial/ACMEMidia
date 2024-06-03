553c2f421b497e64d63ff99646d2717aac81d101
Pedro Tonial <ptpasinato@gmail.com>
2024-06-03 17:20:13 -0300

    Adicionando aexportação ASTAH -> Java

---
+++ src/dados/Midiateca.java
@@ -0,0 +1,56 @@
+package dados;
+
+import java.util.Collection;
+import java.util.ArrayList;
+
+public class Midiateca implements Iterador {
+
+	private int contador;
+
+	private Collection<Midia> midia;
+
+	public Midiateca() {
+
+	}
+
+	public boolean cadastraMidia(Midia jogo) {
+		return false;
+	}
+
+	public Midia consultaPorCodigo(int codigo) {
+		return null;
+	}
+
+	public ArrayList<Midia> consultaPorCategoria(Categoria categoria) {
+		return null;
+	}
+
+	public boolean removeMidia(int codigo) {
+		return false;
+	}
+
+
+	/**
+	 * @see dados.Iterador#reset()
+	 */
+	public void reset() {
+
+	}
+
+
+	/**
+	 * @see dados.Iterador#hasNext()
+	 */
+	public boolean hasNext() {
+		return false;
+	}
+
+
+	/**
+	 * @see dados.Iterador#next()
+	 */
+	public Object next() {
+		return null;
+	}
+
+}
